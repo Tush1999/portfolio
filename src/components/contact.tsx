94f2191987828197962c6c226e1ec9f0e989838a
Tushar <vinsol@192.168.1.3>
2024-05-05 21:04:53 +0530

    added contacts

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -50,8 +50,8 @@ const Contact = () => {
         <p className="career-desc">I enjoy engaging with people in tech, though I may procrastinate on replies. Feel free to start a conversation, and I’ll try my best to respond promptly.</p>
         <form ref={formRef} onSubmit={handleSubmit} className="form-container">
           <div className="name-email-container">
-            <input type="text" name={NAME_FORM_VALUE} value={formValues[NAME_FORM_VALUE]} placeholder="Name" onChange={handleChange} required autoFocus />
-            <input type="email" name={EMAIL_FORM_VALUE} value={formValues[EMAIL_FORM_VALUE]} placeholder="Email" onChange={handleChange} required />
+            <div><input type="text" name={NAME_FORM_VALUE} value={formValues[NAME_FORM_VALUE]} placeholder="Name" onChange={handleChange} required  /></div>
+            <div><input type="email" name={EMAIL_FORM_VALUE} value={formValues[EMAIL_FORM_VALUE]} placeholder="Email" onChange={handleChange} required /></div>
           </div>
           <input type="text" value={formValues[SUBJECT_FORM_VALUE]} name={SUBJECT_FORM_VALUE} placeholder="Subject" onChange={handleChange} required />
           <textarea name={MESSAGE_FORM_VALUE} value={formValues[MESSAGE_FORM_VALUE]} placeholder="Message" onChange={handleChange} required cols={10} rows={5} />
